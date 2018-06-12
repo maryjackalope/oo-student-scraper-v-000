dff774fda3c651701f82af054a85eb645191cb7d
Mary Jack <maryjackinthebox@gmail.com>
2018-06-12 21:21:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,20 +7,20 @@
     @@all << self 
      	     
    end	   
-   end
+   
  	 
-   def self.create_from_collection(students_array)	   def self.create_from_collection(students_array)
+   def self.create_from_collection(students_array)
     students_array.each {|student_hash|
     student = self.new(student_hash)
     student.name = student_hash[:name]
     student.location = student_hash[:location]}
-   end	   end
+   end	   
  	 
-   def add_student_attributes(attributes_hash)	   def add_student_attributes(attributes_hash)
+   def add_student_attributes(attributes_hash)	 
     attributes_hash.each do |key,value|
      self.send(("#{key}="),value)
     end 
-   end	   end
+   end	  
  	 
    def self.all	   def self.all
  @@all  
