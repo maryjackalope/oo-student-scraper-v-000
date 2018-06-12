3f04605428b9474f9bfef2eb623f796a5db87d71
Mary Jack <maryjackinthebox@gmail.com>
2018-06-12 21:38:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,24 +1,42 @@
-@@all = []
+class Student
  	 
-def initialize(student_hash)
-    student_hash.each { |key,value| self.send(("#{key}=") ,value)}
-      @@all << self      	     
+attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+ 	 
+   @@all = []	 
+ 	 
+   def initialize(student_hash)
+@name = student_hash[:name]
+    @location = student_hash[:location]
+    @twitter = student_hash[:twitter]
+    @linkedin = student_hash[:linkedin]
+    @github = student_hash[:github]
+    @blog = student_hash[:blog]
+    @profile_quote = student_hash[:profile_quote]
+    @bio = student_hash[:bio]
+    @profile_url = student_hash[:profile_url]
+    @@all << self
+   end	   
+
+def self.create_from_collection(students_array)
+ students_array.each do |student|
+   new_student = Student.new(student)
 end
- 	 
- def self.create_from_collection(students_array)
- students_array.each {|student_hash|
-student = self.new(student_hash)
-student.name = student_hash[:name]
-student.location = student_hash[:location]}
-   end	  
- 
+ end	 
 
 def add_student_attributes(attributes_hash)
-attributes_hash.each do |key,value|
-self.send(("#{key}="),value)
-end 
-end	  
+self.name = attributes_hash[:name]
+self.location = attributes_hash[:location]
+ self.twitter = attributes_hash[:twitter]
+self.linkedin = attributes_hash[:linkedin]
+self.github = attributes_hash[:github]
+self.blog = attributes_hash[:blog]
+self.profile_quote = attributes_hash[:profile_quote]
+self.bio = attributes_hash[:bio]
+self.profile_url = attributes_hash[:profile_url]
+   end	   
 
 def self.all
-@@all  
-end	
+ @@all
+   end	  
+
+end
